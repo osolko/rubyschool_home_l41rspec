a04c4d4631a6bc7d43fb104b91b0077b31b63859
osolko <o.savka@gmail.com>
2019-06-11 09:17:15 +0300

    add test for health and powerUp

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,10 +1,27 @@
 require './hero'
 
 describe Hero do 
+	
 	it "has a capitalize name" do
-		hero= Hero.new 'mike'
+		hero = Hero.new 'mike'
 
-		expext (hero.name).to eq 'Mike'
+		expect(hero.name).to eq 'Mike'
 	end
+	
+
+	it "has health 100" do
+		hero = Hero.new 'mike' 
+
+		expect(hero.health).to eq 100
+	end
+
+
+	it "can power up" do
+		hero = Hero.new 'mike' 
+
+		expect(hero.power_up).to eq 110
+	end
+
+
 
 end
